f2629723b6ddb0efdb01ebe4685bcdbc53dc6b36
EternalSyntaxErrorAllTime <eternal_syntax_error@mail.ru>
2025-07-09 13:42:42 +0500

    feat(constants): add constValue.ts with suffixData array
    Добавлен файл constValue.ts со статическим массивом `suffixData` — значениями из enum `public.EnumSuffix` для использования в выпадающих списках и формах

---
+++ constValue.ts
@@ -0,0 +1,8 @@
+/**
+ * Переменная в которой хранятся данные для из public.EnumSuffix
+ */
+export const suffixData = [
+    { value: "", label: "ㅤ" },
+    { value: "M", label: "M" },
+    { value: "A", label: "A" },
+  ];
